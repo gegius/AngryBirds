93a1be2d3b37f4280e791848d4e6be4b78f4c5e1
Агеев Глеб Владимирович <glebushka.ageev@gmail.com>
2019-09-16 12:02:38 +0500

    privet

---
+++ AngryBirdsTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		// Ниже — это XML документация, её использует ваша среда разработки, 
+		// чтобы показывать подсказки по использованию методов. 
+		// Но писать её естественно не обязательно.
+		/// <param name="v">Начальная скорость</param>
+		/// <param name="distance">Расстояние до цели</param>
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+		public static double FindSightAngle(double v, double distance)
+		{
+			return 0.5 * Math.Asin((distance * 9.8) / (v * v));
+		}
+
+		static void Main(str[] args)
+		{
+
+		   internal var v = double.Parse(Console.ReadLine());
+		   internal var distance = double.Parse(Console.ReadLine());
+		   Console.WriteLine(FindSightAngle(v, distance));
+	    }
+    }
+
+}
